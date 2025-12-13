62bb7f0b1b513ec54076c8d99f1bff17575474d6
crazyorange49 <84763581+crazyorange49@users.noreply.github.com>
2025-12-13 03:20:08 -0600

    fix: handle case where server is running but no players are online

--- main.py
+++ main.py
@@ -316,6 +316,9 @@ async def online(interaction: discord.Interaction):
             await server_process.stdin.drain()
             await asyncio.sleep(1)
             output = re.sub(r'\x1b\[[0-9;]*m', '', output.decode().strip())
+            if len(output.split("players online: ")) > 1:
+                await interaction.response.send_message("The server is running but no players are online.")
+                return
             number_of_players = output.split("There are ")[1].split(" of a max")[0].strip()
             players = output.split("players online: ")[1].strip().split(", ")
             client_emojis = Emoji_guild.emojis
