6c81d784ad66a4601ae00c87fcca872bb8e98b76
crazyorange49 <84763581+crazyorange49@users.noreply.github.com>
2025-12-13 20:24:27 -0600

    fix: enhance join command to handle voice channel presence and update bot status

--- main.py
+++ main.py
@@ -373,12 +373,16 @@ async def join(interaction: discord.Interaction):
     write_file(message_author, "SLASH COMMAND", interaction.guild, "/join")
     if message_author.voice is None:
         await interaction.response.send_message("you need to be in a vc", ephemeral=True)
-    else:
+    elif joinee is not None:
         voice_channel = message_author.voice.channel
         joinee = message_author
         vc = await voice_channel.connect()
+        game = discord.Game(f"speaking for {joinee}")
+        await client.change_presence(status=discord.Status.online, activity=game)
         await interaction.response.send_message(f"joined {voice_channel}", ephemeral=True)
         print(f" vc id: {voice_channel.id}")
+    else:
+        await interaction.response.send_message("bot is already in use", ephemeral=True)
 
 @client.event
 async def on_voice_state_update(member, before, after):
