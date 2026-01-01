4090c684b7dd5626bd0e53409e31303200d406c1
crazyorange49 <84763581+crazyorange49@users.noreply.github.com>
2026-01-01 00:19:00 -0600

    fix: correct condition in data validation logic of write_data method

--- LLM_Experimental/Data_collection.py
+++ LLM_Experimental/Data_collection.py
@@ -14,7 +14,7 @@ class DataCollector:
             data (list of dict): The data to write to the CSV file.
             mode (str): The file mode, 'a' for append and 'w' for write. Default is 'a'.
         """
-        if not data["message"][0] == "/" or data["message"][0] == "!" or "http" in data["message"]:
+        if data["message"][0] != "/" or data["message"][0] != "!" or "http"  not in data["message"]:
             with open(self.FILE_PATH, mode, newline='') as csvfile:
                 if data:
                     fieldnames = data[0].keys()
